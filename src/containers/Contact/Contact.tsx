6492222c49d6ca1587173d93854ef6a3195bce6e
Ikraam <IbnDaanis@Gmail.com>
2021-04-02 21:21:47 +0100

    Added form

--- src/containers/Contact/Contact.tsx
+++ src/containers/Contact/Contact.tsx
@@ -6,6 +6,27 @@ export const Contact = (): JSX.Element => {
     <section id='contact' className='contact'>
       <div className='container'>
         <h2 className='contact-heading'>Get in contact with me</h2>
+        <div className='contact-form-container'>
+          <form className='contact-form' name='contact' method='POST' data-netlify='true'>
+            <div className='contact-form-item'>
+              <label>Your Name:</label>
+              <input type='text' name='name' />
+            </div>
+            <p>
+              <label>
+                Your Email: <input type='email' name='email' />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name='message'></textarea>
+              </label>
+            </p>
+            <p>
+              <button type='submit'>Send</button>
+            </p>
+          </form>
+        </div>
       </div>
     </section>
   )
