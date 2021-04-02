1a816172ab5ce4c5880ed629270bcf57acc518cb
Ikraam <IbnDaanis@Gmail.com>
2021-04-03 00:20:32 +0100

    Removed images from export

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,2 @@
 export { GithubIcon } from './icons/GithubIcon'
 export { ArrowIcon } from './icons/ArrowIcon'
-
-import Friedrik from './images/friedrik.png'
-import UltraStore from './images/ultrastore.png'
-
-export { Friedrik, UltraStore }
