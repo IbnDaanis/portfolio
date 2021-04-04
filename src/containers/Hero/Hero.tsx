736381c39f3e05b595bd4153b23084cba1f2a8c7
Ikraam <IbnDaanis@Gmail.com>
2021-04-04 19:00:32 +0100

    Removed colour in h1

--- src/containers/Hero/Hero.tsx
+++ src/containers/Hero/Hero.tsx
@@ -10,9 +10,7 @@ export const Hero = (): JSX.Element => {
     <section id='hero' className='hero'>
       <div className='container'>
         <div className='content'>
-          <h1 style={{ transform: `translateY(-${positionY * 0.1}px)` }}>
-            Hello, I&#39;m <span>Ikraam</span>
-          </h1>
+          <h1 style={{ transform: `translateY(-${positionY * 0.1}px)` }}>Hello, I&#39;m Ikraam</h1>
           <h2 style={{ transform: `translateY(-${positionY * 0.05}px)` }}>
             Front-End Developer. Self Taught. From London. Looking forward to enter the Industry.
           </h2>
