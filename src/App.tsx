b2981681d7d92047580be8c5a110ba2b81db47e6
Ikraam <IbnDaanis@Gmail.com>
2021-04-05 03:37:54 +0100

    Check if user is on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import { Header, Scroll } from './components'
 import { Contact, Hero, Projects } from './containers'
+import { userAgentMobile } from './utils/userAgentMobile'
 
 export const App = (): JSX.Element => {
   return (
     <>
       <Header />
-      <Scroll>
-        <Hero />
-        <Projects />
-        <Contact />
-      </Scroll>
+      {userAgentMobile ? (
+        <>
+          <Hero />
+          <Projects />
+          <Contact />
+        </>
+      ) : (
+        <Scroll>
+          <Hero />
+          <Projects />
+          <Contact />
+        </Scroll>
+      )}
     </>
   )
 }
