f3ab979b691f7fa5762d65a28c9a7e42319de38a
Ikraam <IbnDaanis@Gmail.com>
2021-04-05 22:13:04 +0100

    Added custom cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Header, Scroll } from './components'
 import { Contact, Hero, Projects } from './containers'
 import { userAgentMobile } from './utils/userAgentMobile'
@@ -8,8 +8,19 @@ export const App = (): JSX.Element => {
     console.clear()
   }, [])
 
+  const [cursorPosition, setCursorPosition] = useState({ pageY: 0, pageX: 0 })
+  const { pageY, pageX } = cursorPosition
+
+  useEffect(() => {
+    document.addEventListener('mousemove', event => {
+      // console.log(event.target)
+      setCursorPosition({ pageY: event.clientY - 10, pageX: event.clientX - 10 })
+    })
+  }, [])
+
   return (
     <>
+      <div className='cursor' style={{ top: pageY, left: pageX }}></div>
       <Header />
       {userAgentMobile ? (
         <>
