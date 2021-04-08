fc8dfc6b2283c8a154d3ef99133e0c4c7ddd9c95
Ikraam <IbnDaanis@Gmail.com>
2021-04-08 19:37:59 +0100

    Changed classname to refs

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { gsap, ScrollTrigger } from 'gsap/all'
 import './Title.scss'
 import { useWindowSize } from '../../hooks/useWindowSize'
@@ -7,41 +7,49 @@ type text = string
 
 export const Title = ({ title }: { title: text }): JSX.Element => {
   const { width } = useWindowSize()
+
+  const titleEl = useRef<HTMLHeadingElement>(null)
+  const lineEl = useRef<HTMLDivElement>(null)
+
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger)
 
-    const titleTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: `.title-${title.replace(' ', '')}`,
-        start: width < 600 ? 'top 80%' : '50% 90%',
-      },
-    })
+    if (titleEl.current && lineEl.current) {
+      const titleTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: titleEl.current,
+          start: width < 600 ? 'top 80%' : '50% 90%',
+        },
+      })
 
-    titleTl.to(`.title-${title.replace(' ', '')}`, {
-      duration: 1,
-      y: 0,
-      ease: 'power3.out',
-    })
+      titleTl.to([titleEl.current], {
+        duration: 1,
+        y: 0,
+        ease: 'power3.out',
+      })
 
-    const lineTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: `.title-line-${title.replace(' ', '')}`,
-        start: width < 600 ? 'top 80%' : '50% 90%',
-      },
-    })
+      const lineTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: lineEl.current,
+          start: width < 600 ? 'top 80%' : '50% 90%',
+        },
+      })
 
-    lineTl.to(`.title-line-${title.replace(' ', '')}`, {
-      delay: -0.8,
-      duration: 1.5,
-      ease: 'power3.out',
-      width: '100%',
-    })
+      lineTl.to([lineEl.current], {
+        delay: -0.8,
+        duration: 1.5,
+        ease: 'power3.out',
+        width: '100%',
+      })
+    }
   }, [width, title])
 
   return (
     <div className='title-container'>
-      <h2 className={`title title-${title.replace(' ', '')}`}>{title}</h2>
-      <div className={`title-line title-line-${title.replace(' ', '')}`}></div>
+      <h2 className={`title title-${title.replace(' ', '')}`} ref={titleEl}>
+        {title}
+      </h2>
+      <div className={`title-line title-line-${title.replace(' ', '')}`} ref={lineEl}></div>
     </div>
   )
 }
