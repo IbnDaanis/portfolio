282a484e3b976bdb65db676ef16243fd995792b8
Ikraam <IbnDaanis@Gmail.com>
2021-04-09 20:19:01 +0100

    Removed cursor on screenshots

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,11 @@ export const App = (): JSX.Element => {
   const [cursorPosition, setCursorPosition] = useState({ pageY: -30, pageX: -30 })
   const { pageY, pageX } = cursorPosition
 
-  const [pointer, setPointer] = useState(false)
   useEffect(() => {
     document.addEventListener('mouseleave', () => {
       setCursorPosition({ pageY: -55, pageX: -55 })
     })
     document.addEventListener('mousemove', event => {
-      const element: NodeList | null = document.querySelectorAll('.project-image a')
-      if ([...element].some(el => el.contains(event.target as Node)) && event.target && element) {
-        setPointer(true)
-        return setCursorPosition({ pageY: event.clientY - 50, pageX: event.clientX - 50 })
-      }
-      setPointer(false)
       setCursorPosition({ pageY: event.clientY - 10, pageX: event.clientX - 10 })
     })
   }, [])
@@ -55,14 +48,7 @@ export const App = (): JSX.Element => {
           </Scroll>
         )
       ) : null}
-      {!userAgentMobile && (
-        <div
-          className={`cursor ${pointer ? 'cursor-pointer' : ''}`}
-          style={{ top: pageY, left: pageX }}
-        >
-          <span>{pointer && 'Click to view'}</span>
-        </div>
-      )}
+      {!userAgentMobile && <div className='cursor' style={{ top: pageY, left: pageX }}></div>}
     </>
   )
 }
