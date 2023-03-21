cbcb16308fa220d1c7f2637a8460a9c95da7053b
Ikraam <ibndaanis@gmail.com>
2023-03-21 22:04:59 +0000

    styled components ssr

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { ReactNode } from "react";
+import StyledComponentsRegistry from "./registry";
 import { GlobalStyle } from "./styles/globalStyle";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <GlobalStyle />
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
