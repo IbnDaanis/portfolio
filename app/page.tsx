0516d972e9cd04c9f7bc4fc9e95441862d0e52f7
Ikraam <ibndaanis@gmail.com>
2023-04-30 03:21:57 +0100

    meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { App } from "./App";
 
+export const metadata = {
+  title: "Ikraam | Software Engineer",
+  description:
+    "I'm Ikraam, a software engineer from London, United Kingdom. I use the most recent technologies to create front-end web applications."
+};
+
 export default function Page() {
   return <App />;
 }
